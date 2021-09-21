b0838437e11eb0f5b4f13c9951bf17e423a1e1fe
DongHyun Kim <knight2995@kakao.com>
2021-09-21 12:14:26 +0900

    Initial commit

---
+++ src/test/java/duckbo/simpleShop/SimpleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package duckbo.simpleShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
